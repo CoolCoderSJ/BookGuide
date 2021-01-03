1484d02c26355221f1e1d44c10732af993aad016
CoolCoderSJ <53063247+CoolCoderSJ@users.noreply.github.com>
2021-01-03 17:57:48 -0500

    Added separate css and js files to migrate code to later

--- static/js/my_script.js
+++ static/js/my_script.js
@@ -4,6 +4,25 @@ $(document).ready(function() {
 
 var $stars;
 jQuery(document).ready(function ($) {
+  function addScore(score, $domElement) {
+    $("<span class='stars-container'>")
+      .addClass("stars-" + score.toString())
+      .text("★★★★★")
+      .appendTo($domElement);
+  }
+  $.fn.stars = function() {
+    return $(this).each(function() {
+        const rating = $(this).data("rating");
+        const numStars = $(this).data("numStars");
+        const fullStar = '<i class="fas fa-star" style="color: gold;"></i>'.repeat(Math.floor(rating));
+        const halfStar = (rating%1!== 0) ? '<i class="fas fa-star-half-alt" style="color: gold;"></i>': '';
+        const noStar = '<i class="far fa-star" style="color: gold;"></i>'.repeat(Math.floor(numStars-rating));
+        $(this).html(`${fullStar}${halfStar}${noStar}`);
+    });
+}
+$(function(){
+                $('.stars').stars();
+            });
   // Custom whitelist to allow for using HTML tags in popover content
   var myDefaultWhiteList = $.fn.tooltip.Constructor.Default.whiteList
   myDefaultWhiteList.textarea = [];
@@ -14,6 +33,7 @@ jQuery(document).ready(function ($) {
     var stars = document.getElementById('rate');
     stars.value = index;
   });
+  addScore(70, $("#fixture"));
 });
 
 function myFunc(){
@@ -22,3 +42,4 @@ function myFunc(){
     frm.reset();  // Reset all form data
     return false; // Prevent page refresh
   }
+
