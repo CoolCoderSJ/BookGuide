2cd362ee04f40e6610bc49c9f4b2b720a8f17672
Shuchir <53063247+CoolCoderSJ@users.noreply.github.com>
2021-02-10 13:46:28 -0500

    Revert "finished porting to flask"
    This reverts commit a64b3ae1a8a59adb3c7fcb1d89cd3ee25d2ff4ef.

--- main.py
+++ main.py
@@ -1,87 +1,63 @@
-from flask import Flask, render_template, request, redirect, flash
+import web #Import the web.py python framework
 import os #Import os commands
-import sqlite3
-from werkzeug.utils import secure_filename
-from threading import Thread
+render = web.template.render('templates/') #Define where the HTML pages are stored
 
-app = Flask('')
-app.secret_key = '36b4610b69d1acc500fcc8557a3070846f1241c08c37e0d81b33abdf0afb2f0f'
+#Define database connection
+db = web.database(
+    dbn='postgres',
+    host='127.0.0.1',
+    port=5432,
+    user='postgres',
+    pw='postgres',
+    db='webpy_test',
+)
 
-@app.route('/')
-def index(): #Define what happens when Homepagw is visited.
-    conn = sqlite3.connect("collaboreadflask.db")
-    db = conn.cursor()
-    db.execute("SELECT * from books") #Get all books from database
-    books = db.fetchall()
-    db.close()
-    db = conn.cursor()
-    db.execute("SELECT * from reviews") #Get all reviews from database
-    reviews = db.fetchall()
-    db.close()
-    print("BOOKS: "+str(books), "\n\nREVIEWS: "+str(reviews))
-    avgrevs = {}
+#Define all of the possible URLs that can be shown, as well as what method to call when each URL is visited.
+urls = (
+    '/', 'index', #Home page
+    '/new', 'new', #Add the book (NOT THE FORM)
+    '/add', 'add', #Adding a book form
+    '/book_details/(.+)', 'book_details', #GET method for getting book details. Not reccomended, and used in only one place.
+    '/book_details', 'book_details', #POST method for getting book details, more secure/accurate way to get the details.
+    '/review', 'review' #Submit a review method
+    )
 
-    for book in books:
-        db = conn.cursor()
-        db.execute("SELECT * FROM books JOIN reviews ON books.id = reviews.book_id")
-        review_list = db.fetchall()
-        db.close()
-        avg1 = 0
-        avg2 = 0
-        for i in review_list:
-            if i[6]:
-                if int(i[6]) == int(book[0]) and i[-1] != None:
-                    avg1 = avg1 + i[-1]
-                    avg2 = avg2 + 1
-        print(avg2)
-        if avg2 == 0:
-            avg = 0
-        else:
-            avg = avg1/avg2
-        avgrevs[book[1]] = int(avg)
-        print(avgrevs)
-    return render_template("index.html", books=books, reviews=reviews, avgrevs=avgrevs) #Show index.html file, pass the books, reviews, and db (used for database operations) to the html file.
 
-@app.route('/add', methods=["POST"])
-def add(): #Define what happens when a book is added
-    conn = sqlite3.connect("collaboreadflask.db")
-    db = conn.cursor()
-    if request.method == 'POST':
-        i = request.form
-        qr = db.execute("SELECT * from books").fetchall()
-        for book in qr:
-            if book[1].lower() == i['title'].lower():
-                flash("This book already exists.")
-                return redirect("/new")
-        if i['title'] == "":
-            flash("Book Title is a required field.")
-            return redirect("/new")
-        if i['grade'] == "":
-            flash("Appropriate Grde is a required field.")
-            return redirect("/new")
+class index: #Define what happens when Homepagw is visited.
+    def GET(self):
+        global db
+        books = db.select('books') #Get all books from database
+        reviews = db.select('reviews') #Get all reviews from database
+        return render.index(books, reviews, db) #Show index.html file, pass the books, reviews, and db (used for database operations) to the html file.
+
+
+class add: #Define what happens when a book is added
+    def POST(self):
+        i = web.input(myfile={}) # Get user input, make sure image file is a dictionary object so that it is saved properly.
         filedir = 'static/images/books' #Directory where book images are stored
-        file = request.files['myfile']
-        if file.filename == None or file.filename == "": #If no book image is submitted, make the placeholder image the default image.
+        if i.myfile.filename == None or i.myfile.filename == "": #If no book image is submitted, make the placeholder image the default image.
             myfilepath = 'static/images/books/placeholder.png'
         else: #If an image is submitted, do the following:
-            fileext = secure_filename(file.filename.split(".")[-1])
-            myfilepath = filedir + '/' + request.form['title'] + "." + fileext #Define the filepath. This is the directory + the book name + file extension
-            file.save(myfilepath)
+            fileext = i.myfile.filename.split('.')[1] #Get the file extension
+            myfilepath = filedir + '/' + i.title + "." + fileext #Define the filepath. This is the directory + the book name + file extension
+            fout = open(myfilepath, "wb") #Create an empty image object in the directory.
+            fout.write(i.myfile.file.read()) #Write the image contents to the server
+            fout.close() #Safely close the file operation.
         #Insert the book title, author, grade, genre, and image file path in the database
-        db.execute(f"INSERT into books (book_title, author, grade, image) VALUES ('{request.form['title']}', '{request.form['author']}', {request.form['grade']}, '{myfilepath}')")
-        conn.commit()
+        n = db.insert('books', book_title=i.title, author=i.author, grade=i.grade, image=myfilepath)
         # If user wants to go to the indivdual page:
-        if "revnext" in request.form:
-            book = db.execute("SELECT * from books") #Get all books from database
-            bookTitle = request.form["title"] #Get the submitted book title
+        if "revnext" in i:
+            book = db.select('books') #Get all books
+            i = web.input()
+            bookTitle = i.title #Get the submitted book title
             bookDesc = "" #set the description empty, since the book was just added and has no description.
-            bookAuthor = request.form["author"] #Get submitted author
-            qr = db.execute('SELECT * from books WHERE book_title == \''+bookTitle+"'").fetchall() #Using the submitted title, get the book info from database
-            #results = list(qr) #Convert the request to a list so that we can do operations with it
-            bookId = int(qr[0][0]) #Get the book ID from the database response
-            bookGrade = int(qr[0][-1]) #Get the book grade from the database response
-            reviews = db.execute('SELECT * from reviews WHERE book_id == '+str(bookId)).fetchall() #Get all reviews that were submitted for this book
-            #reviews = list(qr2) #Convert to list for operations
+            bookAuthor = i.author #Get submitted author
+            qr = db.select('books', where="book_title=$bookTitle", vars=locals()) #Using the submitted title, get the book info from database
+            results = list(qr) #Convert the request to a list so that we can do operations with it
+            bookId = int(results[0]['id']) #Get the book ID from the database response
+            bookGrade = int(results[0]['grade']) #Get the book grade from the database response
+            qr2 = db.select('reviews', where="book_id=$bookId", vars=locals()) #Get all reviews that were submitted for this book
+            reviews = list(qr2) #Convert to list for operations
             avg_rating1 = 0 #The total star rating. This value will be updated later
             avg_rating2 = 0 #Number of reviews that have stars
             avg = 0 #Average star rating
@@ -95,46 +71,35 @@ def add(): #Define what happens when a book is added
                 avg = avg_rating1/avg_rating2 #Divide total by num of reviews with stars
             bookIdstr =str(bookId) #Convert BookID to string for later use
             #Show the individual page with all of the collected values
-            return redirect("/book_details/"+str(bookId))
+            return render.book_details(book, bookId, bookTitle, bookDesc, bookAuthor, bookGrade, reviews, avg, db, bookIdstr)
         else: #If user does not want to go to the home page then -
-            return redirect('/', code=302) #redirect to home page
+            raise web.seeother('/') #redirect to home page
 
-@app.route('/new', methods=["GET"])
-def new(): #Define what happens when user clicks the "Add a book" button
-    conn = sqlite3.connect("collaboreadflask.db")
-    db = conn.cursor()
-    if request.method == 'GET':
-        book = db.execute("SELECT * from books").fetchall() #Get all books from database (Used to make sure book doesn't already exist)
-        return render_template("new.html", book=book) #Pass the books, db object for operations, and string object for operations to the new book form and show form.
+class new: #Define what happens when user clicks the "Add a book" button
+    def GET(self):
+        global db #Get the database
+        book = db.select('books') #Get all books (Used to make sure book doesn't already exist)
+        return render.new(book, db, str) #Pass the books, db object for operations, and string object for operations to the new book form and show form.
 
-
-@app.route('/book_details/<id>')
-def book_details(id): #Define what happens when user wants to see individual page
-    conn = sqlite3.connect("collaboreadflask.db")
-    db = conn.cursor()
-    if request.method == 'GET':
-        db = conn.cursor()
-        book = db.execute("SELECT * from books").fetchall() #Get all books from database from the database
-        db.close()
-        i = request.form #Get URL parameters, set bookId to None by default in case none is given
-        bookId = int(id) #Convert bookId to integer
-        db = conn.cursor()
-        qr = db.execute('SELECT * from books WHERE id == '+str(bookId)).fetchall() #Get book details from database using the ID to get more details.
-        db.close()
-        print(qr)
-        bookTitle = qr[0][1] #Get the book title from the list
-        bookDesc = qr[0][2] #Get the book description from the list
-        bookAuthor = qr[0][4] #Get the book author from the list
-        bookGrade = qr[0][5] #Get the book grade from the list.
-        db = conn.cursor()
-        reviews = db.execute('SELECT * from reviews WHERE book_id == '+str(bookId)).fetchall() #Get all reviews for the selected book.
-        #reviews = list(qr2) #Convert to list for later operations
+class book_details: #Define what happens when user wants to see individual page
+    def POST(self): #POST method - all data is passed through headers, to reduce spam and show accurate results.
+        book = db.select('books') #Get all books from database
+        i = web.input() #Get "hidden" user input.
+        bookId = int(i.bookId) #Get the book ID. This value is a hidden input value on each card that holds the book id.
+        qr = db.select('books', where="id=$bookId", vars=locals()) #Get book details from database using the ID to get more details.
+        results = list(qr) #Convert to list for further operations.
+        bookTitle = results[0]['book_title'] #Get the book title from the list
+        bookDesc = results[0]['book_description'] #Get the book description from the list
+        bookAuthor = results[0]['author'] #Get the book author from the list
+        bookGrade = results[0]['grade'] #Get the book grade from the list.
+        qr2 = db.select('reviews', where="book_id=$bookId", vars=locals()) #Get all reviews for the selected book.
+        reviews = list(qr2) #Convert to list for further operations
         avg_rating1 = 0 #The total star rating. This value will be updated later
         avg_rating2 = 0 #Number of reviews that have stars
         avg = 0 #Average star rating
         for review in reviews: #For each review in the given reviews, do the following -
-            if review[-1] != None: #If there is a star rating submitted, do the following -
-                avg_rating1 += review[-1] #Add the star to the total stars
+            if review['rating'] != None: #If there is a star rating submitted, do the following -
+                avg_rating1 += review['rating'] #Add the star to the total stars
                 avg_rating2 += 1 #Add one to the total number of reviews that have stars
         if avg_rating2 == 0: #If there were no reviews with stars, set the average to 0.
             avg = 0
@@ -142,43 +107,56 @@ def book_details(id): #Define what happens when user wants to see individual pag
             avg = avg_rating1/avg_rating2 #Divide total by num of reviews with stars
         bookIdstr =str(bookId) #Convert BookID to string for later use
         #Show the individual page with all of the collected values
-        image = "http://localhost:1111/"+qr[0][3]
-        return render_template("book_details.html", book=book, bookId=bookId, bookTitle=bookTitle, bookDesc=bookDesc, bookAuthor=bookAuthor, bookGrade=bookGrade, reviews=reviews, avg=avg, db=db, bookIdstr=bookIdstr, image=image)
+        return render.book_details(book, bookId, bookTitle, bookDesc, bookAuthor, bookGrade, reviews, avg, db, bookIdstr)
 
-@app.route('/review', methods=["POST"])
-def review(): #Define what happens when a review is submitted
-    conn = sqlite3.connect("collaboreadflask.db")
-    db = conn.cursor()
-    if request.method == 'POST':
-        book = db.execute("SELECT * from books").fetchall() #Get all books from database from database
-        db.close()
-        i = request.form #Get user input
-        print(i)
-        if i['review'] == "" and i['stars'] == '0':
-            flash("Please enter either a text description or fill in the stars.")
-            return redirect('/book_details/'+i['bookId'], code=303)
-        else:
-            bookId = int(i['bookId']) #Get bookId (Hidden input)
-            Desc = i['review'] #Get text review
-            name = i['name'] #Get reviewer name
+    def GET(self):
+        book = db.select('books') #Get all books from the database
+        i = web.input(bookId=None) #Get URL parameters, set bookId to None by default in case none is given
+        bookId = int(i.bookId) #Convert bookId to integer
+        qr = db.select('books', where="id=$bookId", vars=locals()) #Get book details from database using the ID to get more details.
+        results = list(qr) #Convert to list for further operations.
+        bookTitle = results[0]['book_title'] #Get the book title from the list
+        bookDesc = results[0]['book_description'] #Get the book description from the list
+        bookAuthor = results[0]['author'] #Get the book author from the list
+        bookGrade = results[0]['grade'] #Get the book grade from the list.
+        qr2 = db.select('reviews', where="book_id=$bookId", vars=locals()) #Get all reviews for the selected book.
+        reviews = list(qr2) #Convert to list for later operations
+        avg_rating1 = 0 #The total star rating. This value will be updated later
+        avg_rating2 = 0 #Number of reviews that have stars
+        avg = 0 #Average star rating
+        for review in reviews: #For each review in the given reviews, do the following -
+            if review['rating'] != None: #If there is a star rating submitted, do the following -
+                avg_rating1 += review['rating'] #Add the star to the total stars
+                avg_rating2 += 1 #Add one to the total number of reviews that have stars
+        if avg_rating2 == 0: #If there were no reviews with stars, set the average to 0.
+            avg = 0
+        else: #If there were star ratings, do the following -
+            avg = avg_rating1/avg_rating2 #Divide total by num of reviews with stars
+        bookIdstr =str(bookId) #Convert BookID to string for later use
+        #Show the individual page with all of the collected values
+        return render.book_details(book, bookId, bookTitle, bookDesc, bookAuthor, bookGrade, reviews, avg, db, bookIdstr)
 
-            db = conn.cursor()
-            qr = db.execute("SELECT * from books WHERE id = '"+str(bookId)+"'").fetchall() #Get book details for specific book from database
-            db.close()
-            bookTitle = qr[0][1] #Get the book title from the list
-            bookDesc = qr[0][2] #Get the book description from the list
-            bookAuthor = qr[0][4] #Get the book author from the list
-            bookGrade = qr[0][5] #Get the book grade from the list.
-            stars = i['stars'] #Get stars value
-            db = conn.cursor()
-            db.execute(f"INSERT into reviews (book_id, name, description, rating) VALUES ('{i['bookId']}',  '{i['name']}', '{i['review']}', '{i['stars']}')") #Insert text review, reviewer name, bookId, and star rating into datbase
-            conn.commit()
-            db.close()
-            db = conn.cursor()
-            reviews = db.execute('SELECT * from reviews WHERE book_id == '+str(bookId)).fetchall() #Get reviews for specific book
-            db.close()
-            #reviews = list(qr2) #Convert to list for operations
-            bookId2 = str(bookId)
-            return redirect('/book_details/'+bookId2, code=303) #Reload the individual page/reviews
+class review: #Define what happens when a review is submitted
+    def POST(self):
+        book = db.select('books') #Get all books from database
+        i = web.input() #Get user input
+        bookId = int(i.bookId) #Get bookId (Hidden input)
+        Desc = i.review #Get text review
+        name = i.name #Get reviewer name
+        qr = db.select('books', where="id=$bookId", vars=locals()) #Get book details for specific book from database
+        results = list(qr) #Convert response to list for operations
+        bookTitle = results[0]['book_title'] #Get the book title from the list
+        bookDesc = results[0]['book_description'] #Get the book description from the list
+        bookAuthor = results[0]['author'] #Get the book author from the list
+        bookGrade = results[0]['grade'] #Get the book grade from the list.
+        stars = i.stars #Get stars value
+        db.insert('reviews', book_id=i.bookId, name=i.name, description=i.review, rating = i.stars) #Insert text review, reviewer name, bookId, and star rating into datbase
+        qr2 = db.select('reviews', where="book_id=$bookId", vars=locals()) #Get reviews for specific book
+        reviews = list(qr2) #Convert to list for operations
+        bookId2 = str(bookId)
+        raise web.seeother('/book_details?bookId='+bookId2) #Reload the individual page/reviews
 
-app.run(host="0.0.0.0", port=1111)
+
+if __name__ == "__main__": #If current file is run -
+    app = web.application(urls, globals()) #Define the web server (this is a webpy feature)
+    app.run() #Start the server
